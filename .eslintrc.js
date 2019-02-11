5ad9fc8c3202a186448e71ee35aa2ad9acaf5b26
glaucia86 <glaucia_lemos86@hotmail.com>
2019-02-11 21:46:40 -0200

    initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+      "import"
+    ],
+    "rules": {
+      "no-console": "off",
+      "no-unused-vars": "off",
+      "max-len": 0
+    }
+};
