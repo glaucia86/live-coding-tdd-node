b3abf10161193f94951d6043ca766b939aa54c62
glaucia86 <glaucia_lemos86@hotmail.com>
2019-03-03 19:06:40 -0300

    Instalacao dos pacotes no projeto.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-      "import"
-    ],
-    "rules": {
-      "no-console": "off",
-      "no-unused-vars": "off",
-      "max-len": 0,
-      "prefer-destructuring": "off",
-      "import/newline-after-import": "off",
-      "no-unused-expressions": "off",
-      "no-undef": "off",
-      "object-shorthand": "off"
-    }
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "no-console": "off",
+    "import/newline-after-import": "off",
+    "no-unused-vars": 0,
+    "max-len": 0,
+    "prefer-destructuring": "off",
+    "no-unused-expressions": "off",
+    "no-undef": "off",
+    "object-shorthand": "off",
+    "global-require": "off",
+    "consistent-return": "off",
+    "no-shadow": "off"
+  }
 };
